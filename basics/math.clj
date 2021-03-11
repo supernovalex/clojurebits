f7236f0c667da02d3c914d3ee9bcc01497941ba3
Alexander Murphy <supernova@alexmurphy.uk>
2021-03-11 22:15:33 +0000

    clojure pretty neat

---
+++ basics/math.clj
@@ -0,0 +1,19 @@
+; comments start with a semi colon, unusual!
+
+; the namespace should be set first
+(ns clojuremath)
+
+; straightforward operations
+(+ 1 1)
+(- 2 8)
+(* 4 5)
+(/ 10 2)
+
+; boolean logic
+(= 1 1)
+(= 2 1)
+
+; a print macro
+(println "Hello there world")
+
+
---
+++ learn_x_in_y_closure/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to learn_x_in_y_closure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
