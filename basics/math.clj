c470c432a83dbafa5fc42bb1ce19b5658c56f33b
alex desktop <supernova@alexmurphy.uk>
2021-03-12 16:02:53 +0000

    desktop commit

--- basics/math.clj
+++ basics/math.clj
@@ -1,19 +1,17 @@
 ; comments start with a semi colon, unusual!
 
-; the namespace should be set first
-(ns clojuremath)
+	; the namespace should be set first
+(ns basics)
 
-; straightforward operations
-(+ 1 1)
-(- 2 8)
-(* 4 5)
+	; straightforward operations
+	(+ 1 1)
+	(- 2 8)
+	(* 4 5)
 (/ 10 2)
 
-; boolean logic
-(= 1 1)
+	; boolean logic
+	(= 1 1)
 (= 2 1)
 
-; a print macro
-(println "Hello there world")
-
-
+	; a print macro
+	(println "Hello there world")
