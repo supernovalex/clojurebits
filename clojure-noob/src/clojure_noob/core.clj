d4bf0dc227f99ccb71c9f87b720343d54112d10e
Alexander Murphy <supernova@alexmurphy.uk>
2021-03-28 21:55:38 +0100

    vim practise

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -1,3 +1,4 @@
+; vim practice too
 (ns clojure-noob.core
   (:gen-class))
 
@@ -5,3 +6,13 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "I'm a little teapot!"))
+
+
+; find this with grep
+
+  ; these lines were 
+  ; all indented
+  ; simultaneously with the
+  ; > command
+
+; this is a really long line so you want to use the 0 key to reach the first character as fast as possible
