c4147c3206d4ed942ae343a989f7421f15740148
Alexander Murphy <supernova@alexmurphy.uk>
2021-04-06 16:53:45 +0100

    v1_clicker doesn't work now

---
+++ v1_clicker/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to v1_clicker
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ v1_clicker/src/v1_clicker/core.clj
@@ -0,0 +1,7 @@
+(ns v1-clicker.core
+  (:require [libpython-clj.python :as py]))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
