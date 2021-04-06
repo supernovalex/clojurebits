c9e1291ed633e4f18b1a5688fc61df7a3a6b8573
Alexander Murphy <supernova@alexmurphy.uk>
2021-04-06 22:33:27 +0100

    installing other libraries

--- v1_clicker/src/v1_clicker/core.clj
+++ v1_clicker/src/v1_clicker/core.clj
@@ -1,7 +1,5 @@
-(ns v1-clicker.core
-  (:require [libpython-clj.python :as py]))
+(ns v1_clicker.core)
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!" (reduce str args)))
