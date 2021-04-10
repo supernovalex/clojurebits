adafe4d60d3ff66e9b1328399b93a7f9aa8bc028
supernovalex <supernova@alexmurphy.uk>
2021-04-10 21:21:41 +0100

    web app (hello there server)

---
+++ web_app_tut/friendwall/project.clj
@@ -0,0 +1,11 @@
+(defproject friendwall "0.1.0-SNAPSHOT"
+  :description "Demo app for a small social network"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring "1.8.0"]]
+  :main ^:skip-aot friendwall.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
---
+++ web_app_tut/friendwall/test/friendwall/core_test.clj
@@ -0,0 +1,7 @@
+(ns friendwall.core-test
+  (:require [clojure.test :refer :all]
+            [friendwall.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
