724b5d8dad5b39210fe6eeefb251e9372ee6c4c4
Alexander Murphy <supernova@alexmurphy.uk>
2021-06-15 14:05:50 +0100

    quoting in clojure

---
+++ basics/quotes-in-clojure.clj
@@ -0,0 +1,8 @@
+; this is the stanard quote macro
+(quote (1 2))
+
+; this is shorthand
+'(1 2)
+
+; one way to understand the quote macro is "it tells the reader to leave the data structure as-is"
+; instead of to treate it like pre-fix function notation
